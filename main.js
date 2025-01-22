9e86b8093308c79c6cbd5caba38c38db30cbfd67
aldafernandez <aldana.fernandez@outlook.com.ar>
2025-01-22 19:18:20 -0300

    .

--- main.js
+++ main.js
@@ -5,10 +5,4 @@ let navMenu = document.querySelector(".nav-menu");
 
 let drawer = document.querySelector(".drawer");
 
-openMenu.addEventListener('click', () => {
-    navMenu.classList.add('drawer');
-});
 
-closeMenu.addEventListener('click', () => {
-    navMenu.classList.remove('drawer');
-})
